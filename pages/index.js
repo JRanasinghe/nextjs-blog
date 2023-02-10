592d645fda1029939dc9ad8ce465e6d1a1bb2573
Janaka Ranasinghe <jranasinghe@gmail.com>
2023-02-10 17:19:27 +0000

    fixed the links

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home({allPostsData}) {
         <u1 className={utilStyles.list}>
           {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={'/posts/${id}'}>{title}</Link>
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
